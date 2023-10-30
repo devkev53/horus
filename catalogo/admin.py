461f0cf01e4d4564a111059bb47fb748d3ee827d
Kevin Palma <root@DESKTOP-9D5BBSU.>
2023-10-29 18:55:47 -0600

    :pencil2: Fix a vision
    Fix a vision

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -18,11 +18,4 @@ class ProductAdmin(admin.ModelAdmin):
   list_display = ('id', 'preview_img', 'name', 'stock')
   list_filter = ('category_id',)
   list_display_links = ('preview_img', 'name',)
-  # inlines = [
-  #   Inline,
-  # ]
-  # raw_id_fields = ('',)
-  # readonly_fields = ('',)
   search_fields = ('name', 'category_id__name')
-  # date_hierarchy = ''
-  # ordering = ('',)
